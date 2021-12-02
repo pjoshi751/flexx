fae0878f2a6a2a68475aaf688539cbc91656b426
Puneet Joshi <puneet@mosip.io>
2021-12-02 21:18:05 +0530

    WIP.

--- resident.py
+++ resident.py
@@ -13,26 +13,31 @@ flx.assets.associate_asset(__name__, 'style.css', style)
 class Resident(flx.Widget):
 
     def init(self):
-        with flx.VBox():
-            with flx.HBox(css_class='title'):
-                flx.Label(text='Resident HelpDesk')
+        with flx.VBox(css_class='form'):
+            with flx.HBox():
+                flx.Label(text='Resident HelpDesk', css_class='title')
                 flx.ImageWidget(flex=1, source='https://www.omidyarnetwork.in/wp-content/uploads/2019/05/mosip.png')
             with flx.HBox():
                 with flx.VBox():
-                    self.buta = flx.Button(text='red')
-                    self.butb = flx.Button(text='green')
-                    self.butc = flx.Button(text='blue')
+                    self.labela = flx.Label(text='RID status', css_class='left_label_selected')
+                    self.labelb = flx.Label(text='green', css_class='left_label')
+                    self.labelc = flx.Label(text='blue', css_class='left_label')
                     flx.Widget(flex=1)  # space filler
                 with flx.StackLayout(flex=1) as self.stack:
-                    self.buta.w = flx.Widget(style='background:#a00;')
-                    self.butb.w = flx.Widget(style='background:#0a0;')
-                    self.butc.w = flx.Widget(style='background:#00a;')
+                    self.labela.w = flx.Widget(style='background:#a00;')
+                    self.labelb.w = flx.Widget(style='background:#0a0;')
+                    self.labelc.w = flx.Widget(style='background:#00a;')
             flx.Label(text='(c) MOSIP www.mosip.io')
+        self.current_label = self.labela
 
-    @event.reaction('buta.pointer_down', 'butb.pointer_down', 'butc.pointer_down')
+    @event.reaction('labela.pointer_down', 'labelb.pointer_down', 'labelc.pointer_down')
     def _stacked_current(self, *events):
-        button = events[-1].source
-        self.stack.set_current(button.w)
+        cur = self.current_label
+        cur.set_css_class('left_label')  # Reset the color
+        cur  = events[-1].source  # New selected label
+        self.stack.set_current(cur.w)
+        cur.set_css_class('left_label_selected')
+        self.current_label = cur
 
 def main(argv):
 
