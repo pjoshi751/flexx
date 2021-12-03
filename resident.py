e3cb02bce199bdbbf113b73c3ff218b04493c1b8
Puneet Joshi <puneet@mosip.io>
2021-12-03 18:39:44 +0530

    WIP.

--- resident.py
+++ resident.py
@@ -10,12 +10,29 @@ with open('style.css') as f:
 
 flx.assets.associate_asset(__name__, 'style.css', style)
 
+class ResidentMain(flx.PyComponent):
+
+    def __init__(self, *args, **kwargs):
+        self.__kwargs = kwargs
+        super().__init__(*args, **kwargs)
+
+    def init(self):
+        super().init()
+        self.resident = Resident(**self.__kwargs)
+
+    @flx.reaction('resident.rid_submitted')
+    def handle_rid_submitted(self, *events):
+        rid = events[-1]['rid'] 
+        status = get_rid_status(rid)
+        self.resident.set_status(f'Status: {status}')
+        get_credential_types()
+
 class Resident(flx.Widget):
 
     def init(self):
         with flx.VBox():
             with flx.HBox():
-                flx.Label(text='Resident HelpDesk', css_class='sitetitle border')
+                flx.Label(text='Resident HelpDesk', css_class='sitetitle')
                 flx.ImageWidget(flex=1, source='https://www.omidyarnetwork.in/wp-content/uploads/2019/05/mosip.png', css_class='logo')
             with flx.HBox():
                 with flx.VBox():
@@ -26,7 +43,12 @@ class Resident(flx.Widget):
                     self.label_e = flx.Label(text='Auth history', css_class='left_label')
                     flx.Widget(flex=1)  # space filler
                 with flx.StackLayout(flex=1) as self.stack:
-                    self.label_a.w = flx.Widget(style='background:#fff;')
+                    with flx.FormLayout(css_class='form') as self.label_a.w:
+                        self.subtitle = flx.Label(text='RID status', css_class='subtitle')
+                        self.rid = \
+                          flx.LineEdit(title='RID', text='')
+                        self.submit = flx.Button(text='Submit')
+
                     self.label_b.w = flx.Widget(style='background:#fff')
                     self.label_c.w = flx.Widget(style='background:#fff;')
                     self.label_d.w = flx.Widget(style='background:#fff;')
@@ -49,7 +71,7 @@ def main(argv):
 
     unused = argv # noqa
 
-    app = flx.App(Resident,
+    app = flx.App(ResidentMain,
                   title='Resident Portal') # , icon=ico)
     # app.export('test.html', link=0)
     app.serve('')
