427af92f44ffa1c67764b888f59fe0fe9c35231b
Puneet Joshi <puneet@mosip.io>
2021-12-03 17:21:59 +0530

    WIP.

--- resident.py
+++ resident.py
@@ -15,23 +15,28 @@ class Resident(flx.Widget):
     def init(self):
         with flx.VBox():
             with flx.HBox():
-                flx.Label(text='Resident HelpDesk', css_class='sitetitle')
+                flx.Label(text='Resident HelpDesk', css_class='sitetitle border')
                 flx.ImageWidget(flex=1, source='https://www.omidyarnetwork.in/wp-content/uploads/2019/05/mosip.png', css_class='logo')
             with flx.HBox():
                 with flx.VBox():
-                    self.labela = flx.Label(text='RID status', css_class='left_label_selected')
-                    self.labelb = flx.Label(text='green', css_class='left_label')
-                    self.labelc = flx.Label(text='blue', css_class='left_label')
+                    self.label_a = flx.Label(text='RID status', css_class='left_label_selected')
+                    self.label_b = flx.Label(text='Auth lock', css_class='left_label')
+                    self.label_c = flx.Label(text='eCard', css_class='left_label')
+                    self.label_d = flx.Label(text='VID', css_class='left_label')
+                    self.label_e = flx.Label(text='Auth history', css_class='left_label')
                     flx.Widget(flex=1)  # space filler
                 with flx.StackLayout(flex=1) as self.stack:
-                    self.labela.w = flx.Widget(style='background:#fff;')
-                    self.labelb.w = flx.Widget(style='background:#fff')
-                    self.labelc.w = flx.Widget(style='background:#fff;')
+                    self.label_a.w = flx.Widget(style='background:#fff;')
+                    self.label_b.w = flx.Widget(style='background:#fff')
+                    self.label_c.w = flx.Widget(style='background:#fff;')
+                    self.label_d.w = flx.Widget(style='background:#fff;')
+                    self.label_e.w = flx.Widget(style='background:#fff;')
             flx.Label(text='(c) MOSIP www.mosip.io', css_class='sitefooter')
 
-        self.current_label = self.labela
+        self.current_label = self.label_a
 
-    @event.reaction('labela.pointer_down', 'labelb.pointer_down', 'labelc.pointer_down')
+    @event.reaction('label_a.pointer_down', 'label_b.pointer_down', 'label_c.pointer_down', 'label_d.pointer_down',
+                    'label_e.pointer_down')
     def _stacked_current(self, *events):
         cur = self.current_label
         cur.set_css_class('left_label')  # Reset the color
